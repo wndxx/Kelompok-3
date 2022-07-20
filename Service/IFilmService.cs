6761f44654d55f9323678c1b85727b6e634e5200
Shandy Destiawan <shandy.destiawan17@gmail.com>
2022-07-20 20:55:56 +0700

    tambahkan interface
    Signed-off-by: Shandy Destiawan <shandy.destiawan17@gmail.com>

---
+++ Service/IFilmService.cs
@@ -0,0 +1,14 @@
+﻿using UASPemrograman2.ModelDB;
+namespace UASPemrograman2.Service
+{
+    public interface IFilmService
+    {
+        public Task<List<Film>> GetFilm();
+
+        public void AddFilm(Film Fl);
+        public void DelFilm(Film Fl);
+        public void UpdFilm(Film Fl);
+
+
+    }
+}
---
+++ Service/IPembeliService.cs
@@ -0,0 +1,14 @@
+﻿using UASPemrograman2.ModelDB;
+namespace UASPemrograman2.Service
+{
+    public interface IPembeliService
+    {
+        public Task<List<Pembeli>> GetPembeli();
+
+        public void AddPembeli(Pembeli Pmb);
+        public void DelPembeli(Pembeli Pmb);
+        public void UpdPembeli(Pembeli Pmb);
+
+
+    }
+}
